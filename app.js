6350403c8697a78ecdb8133a3b23828d033a8000
Isaac lee <izerglee@gmail.com>
2023-07-23 22:23:51 +0800

    learning new conditionals

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
-let cash = 30
-let price = 40
-let change = cash - price
+let cash = 50
+let price = 40 
+let isStoreOpen = true
 
-if (cash > price) {
-   console.log(`you paid extra - here's ${change} dollars change`)
-}
-else if (cash === price) {
-    console.log("you paid the exact amount, have a nice day!")
-}
-else  {
-    console.log(`not enough money - you still owe ${change} dollars`)
-}
+let str = cash >= price && isStoreOpen === true 
+? ('give receipt') 
+: ('Do not give receipt')
+console.log(str)
