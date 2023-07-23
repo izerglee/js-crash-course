aaacea36964ea574aa2ab97f1bbd832b7d8ee6d4
Isaac lee <izerglee@gmail.com>
2023-07-23 22:49:49 +0800

    string loop in crash course trials

--- app.js
+++ app.js
@@ -1,17 +1,5 @@
- for( i = 1; i <= 20; ++i) {
-     if (i % 3 === 0 && i % 5 === 0) {
-         console.log(`${i} -> Frontend Simplified`)
-     }
-     
-    else if (i % 3 === 0)  {
-    console.log(`${i} -> Frontend`)
-    }
+const str = "Frontend Simplified"
 
-    else if (i % 5 === 0) {
-        console.log(`${i} -> Simplified`)
-    }
-else { 
-    console.log(`${i}`)
+for (let i = 0; i < str.length; ++i) {
+    console.log(str[i])
 }
-
- }
