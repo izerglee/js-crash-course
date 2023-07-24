81daf9d52e9eb2490656e0ed40607e571c6e9671
Isaac lee <izerglee@gmail.com>
2023-07-25 00:25:30 +0800

    learning funcitons

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-const str = "Frontend Simplified"
-
-for (let i = 0; i < str.length; ++i) {
-    console.log(str[i])
+function welcomePersonToFes(name) {
+    console.log(`Welcome to FES, ${name}`)
+    
 }
+
+welcomePersonToFes('Isaac');
