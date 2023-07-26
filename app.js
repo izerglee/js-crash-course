0177f99ddbf1baf4430c90de478bbe6c67d89ac7
izerglee <izerglee@gmail.com>
2023-07-26 09:14:23 +0800

    Doing beginner challenge practice number 3

--- app.js
+++ app.js
@@ -1,17 +1,13 @@
-function seconds(hour) {
-    return hour * 3600;
+// function seconds(hour) {
+//     return hour * 3600;
+// }
+
+// console.log(seconds(2));
+// console.log(seconds(10));
+// console.log(seconds(24));
+
+function perimeter (num1, num2) {
+    return (num1 + num2) * 2
 }
 
-console.log(seconds(2));
-
-function seconds(hour) {
-    return hour * 3600;
-}
-
-console.log(seconds(10));
-
-function seconds(hour) {
-    return hour * 3600;
-}
-
-console.log(seconds(24));
+console.log(perimeter(2, 9))
