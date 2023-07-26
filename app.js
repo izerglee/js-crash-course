fde70ef079dae005157efaf3eafa07dc1e0b5543
Isaac lee <izerglee@gmail.com>
2023-07-27 00:44:34 +0800

    finished question 11

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-function calcRemainder (num1, num2) {
-    return num1 % num2 
+// function calcRemainder (num1, num2) {
+//     return num1 % num2 
+// }
+
+// console.log(calcRemainder(4, 2))
+// console.log(calcRemainder(7, 8))
+// console.log(calcRemainder(9, 8))
+
+function isOdd (num) {
+return num % 2 !== 0
 }
 
-console.log(calcRemainder(4, 2))
-console.log(calcRemainder(7, 8))
-console.log(calcRemainder(9, 8))
+console.log(isOdd(1))
+console.log(isOdd(2))
+console.log(isOdd(3))
