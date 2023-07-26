ee4a19d76bf9e1aecd6729396eee9e25b1116403
Isaac lee <izerglee@gmail.com>
2023-07-27 00:52:02 +0800

    finished question 12

--- app.js
+++ app.js
@@ -6,10 +6,18 @@
 // console.log(calcRemainder(7, 8))
 // console.log(calcRemainder(9, 8))
 
-function isOdd (num) {
-return num % 2 !== 0
+// function isOdd (num) {
+// return num % 2 !== 0
+// }
+
+// console.log(isOdd(1))
+// console.log(isOdd(2))
+// console.log(isOdd(3))
+
+function booleanInteger(num) {
+  return  num % 2 === 0 ? 1 : -1
 }
 
-console.log(isOdd(1))
-console.log(isOdd(2))
-console.log(isOdd(3))
+console.log(booleanInteger(1))
+console.log(booleanInteger(2))
+console.log(booleanInteger(5))
