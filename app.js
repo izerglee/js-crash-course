a71da393416673d4c16347416088fbc089dd8bf9
izerglee <izerglee@gmail.com>
2023-07-26 10:19:23 +0800

    finished questions 3-5

--- app.js
+++ app.js
@@ -6,8 +6,22 @@
 // console.log(seconds(10));
 // console.log(seconds(24));
 
-function perimeter (num1, num2) {
-    return (num1 + num2) * 2
+// function perimeter (num1, num2) {
+//     return (num1 + num2) * 2
+// }
+
+// console.log(perimeter(2, 9))
+
+// function triangleArea (base, height) {
+//     return 0.5 * (base * height)
+// }
+
+// console.log(triangleArea(20, 20))
+
+function appendFrontend (string) {
+    return 'Frontend' + string
 }
 
-console.log(perimeter(2, 9))
+console.log(appendFrontend('Apple'))
+console.log(appendFrontend('Banana'))
+console.log(appendFrontend('Orange'))
